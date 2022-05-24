1e62b2b1008adbd22b4106af52674f4d884f419f
Drunkaroos <106116310+Drunkaroos@users.noreply.github.com>
2022-05-23 22:28:30 -0600

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -100,8 +100,8 @@ def tracker():
                 text += f"\n**Date Time:** `{timestampToHumanReadble(transaction.date)}`"
                 text += "\n**Amount:** `{} {}` \n**Price:** `{} ETH (${})`\n**Price/Token:** `${}`".format(*getAmounts(transaction, getTransactionType(transaction)[1]))
                 keyboard = [[Button.url(text='Etherscan', url=F"https://etherscan.io/tx/{transaction.id}"),Button.url(text=f"Buy On Uniswap", url=f"https://app.uniswap.org/#/swap?outputCurrency={CONTRACT}&chain=mainnet")]]
-                chat = BOT.get_entity(CHANNEL)
-                BOT.send_message(chat, message=text,buttons=keyboard)
+                chat = await BOT.get_entity(CHANNEL)
+                await BOT.send_message(chat, message=text,buttons=keyboard)
                 time.sleep(5)
                 
             else:
